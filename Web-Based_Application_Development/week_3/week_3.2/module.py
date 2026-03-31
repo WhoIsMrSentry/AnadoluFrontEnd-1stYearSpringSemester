66c81efc07cea7e9d75c54895fa5f19f28db9bcb
SentryCoderDev <134494889+SentryCoderDev@users.noreply.github.com>
2026-03-31 21:34:02 +0300

    Add module example and parameter usage demo (week 3.2)

---
+++ Web-Based_Application_Development/week_3/week_3.2/module.py
@@ -0,0 +1,17 @@
+import math
+print(math.sqrt(16))
+
+
+#kendi modulumuzu oluşturalım
+# içeriği 
+
+
+# def kare_al(sayi):
+#     return sayi ** 2
+
+# sonuc1 = kare_al(5)
+# sonuc2 = kare_al(10)
+
+# print(sonuc1 + sonuc2)
+
+from square import kare_al
---
+++ Web-Based_Application_Development/week_3/week_3.2/parameter.py
@@ -0,0 +1,5 @@
+def selamla(isim, mesaj="Hoş geldiniz!"):
+    print(f"{isim} {mesaj}")
+
+selamla("Ahmet")
+selamla("Ayşe", "Laboratuvara hoş geldiniz!")
