b10c3ef803ffdbae335e45531a050fa657251541
SentryCoderDev <134494889+SentryCoderDev@users.noreply.github.com>
2026-04-03 19:32:28 +0300

    feat(week-5.1): add exercise 5.1 — basic HTML and interactive script

---
+++ Advanced_Web_Design/week_5/week_5.1/script.js
@@ -0,0 +1,11 @@
+var baslik = document.getElementById("baslik");
+console.log("getElementById: ", baslik );
+
+var kutular = document.getElementsByClassName("kutu");
+console.log("getElementsByClassName: ", kutular);
+
+var ilkvurgulu = document.querySelector(".vurgulu");
+console.log("querySelector: ", ilkvurgulu);
+
+var tumvurgulular = document.querySelectorAll(".vurgulu");
+console.log("querySelectorAll: ", tumvurgulular);
