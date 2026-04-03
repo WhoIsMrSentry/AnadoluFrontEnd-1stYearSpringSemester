ca738b98f54209275b133981eacb52998a240270
SentryCoderDev <134494889+SentryCoderDev@users.noreply.github.com>
2026-04-03 19:32:35 +0300

    feat(week-5.6): add exercise 5.6 — DOM manipulation examples

---
+++ Advanced_Web_Design/week_5/week_5.6/script.js
@@ -0,0 +1,14 @@
+var tümürünler = document.querySelectorAll(".urun");
+
+for (var urun of tümürünler) {
+    var stokMiktarı = parseInt(urun.getAttribute("data-stok"));
+
+    if(stokMiktarı == 0) {
+        //ürün yoksa gizle
+        urun.classList.add("gizle");
+    } else {
+        urun.classList.add("mevcut");
+        console.log(urun.innerText, "stokta: ", stokMiktarı);
+        urun.textContent = urun.textContent + " (stokta: " + stokMiktarı + ")";
+    }
+}
